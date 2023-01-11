9ba1c1f8caa29a919266902614680fda8be27fe6
haeun <shy624@naver.com>
2023-01-11 11:49:13 +0900

    vue prj start

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app>
+    <!-- 앱바 테마를 primary로 설정 -->
+    <v-app-bar app color="primary" dark>
+      <!-- 좌측에 메뉴 아이콘 생성 -->
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>멀티 페이지</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-main>
+      <!-- 페이지 장면전환 효과 -->
+      <v-slide-x-transition mode="out-in">
+        <router-view></router-view>
+      </v-slide-x-transition>
+    </v-main>
+    <!-- Footer 영역 테마를 secondary로 설정 -->
+    <v-footer color="secondary" fixed dark>
+      <div class="mx-auto">Create By VueJS</div>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/main_page.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-container>
+        <p class="display-1 my-4">메인 페이지입니다.</p>
+        <v-divider></v-divider>
+        <p class="display-4 my-4">main</p>
+        <div class="text-center">
+            <!-- 버튼에서 fab, large 어트리뷰트 사용해서 큰 원으로 설정 -->
+        <v-btn fab large class="mt-5" color="purple" dark to="/sub">
+        <v-icon>mdi-arrow-right</v-icon>
+        </v-btn>
+        </div>
+    </v-container>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
